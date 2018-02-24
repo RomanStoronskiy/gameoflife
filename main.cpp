76de2cbd2409926af1462fd45c318ea3a9c5aad2
Danny <liquidcore7@gmail.com>
2018-02-24 21:02:29 +0200

    skeleton added
    Signed-off-by: Danny <liquidcore7@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Playground.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    size_t size;
+    std::cout << "Enter field size: ";
+    std::cin >> size;
+
+    Playground playingField = Playground(size);
+    char i = 'y';
+    do {
+        playingField.print();
+        std::cout << std::endl;
+        std::cout << "Click [y] for next move, [n] to exit; ";
+        std::cin >> i;
+        playingField.nextMove();
+    }
+    while (i != 'n');
     return 0;
 }
